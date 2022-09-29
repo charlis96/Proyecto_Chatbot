1335744081cc4320abf77ff481de22960e7efff9
Your Name <bruno.bugarin@correo.ucu.edu.uy>
2022-09-28 21:08:42 -0500

    s

--- src/Library/Location.cs
+++ src/Library/Location.cs
@@ -1,8 +1,12 @@
 namespace Library
 {
+    /// <summary>
+    ///  Esta clase es utilizada con el propsoito de saber la ubicacion de por ejemplo un empleo.
+    /// </summary>
     public class Location
     {
         public int EjeX { get; set; }
         public int EjeY { get; set; }
+        public int EjeZ { get; set; }
     }
 }
