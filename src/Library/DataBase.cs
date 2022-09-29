f1a441bc3428be082c06261b0bf85f60fe886c1b
Your Name <bruno.bugarin@correo.ucu.edu.uy>
2022-09-28 21:18:57 -0500

    s

--- src/Library/DataBase.cs
+++ src/Library/DataBase.cs
@@ -7,6 +7,11 @@ namespace Library
     public class DataBase
     {
         private static DataBase data;
+        private DataBase()
+        {
+
+        }
+    
         public static DataBase Data
         {
             get
@@ -18,6 +23,7 @@ namespace Library
                 return data;
             }
         }
+        
         public List<Category> Categories { get; set; } = new List<Category>();
 
         public List<Employee> Employees { get; set; } = new();
